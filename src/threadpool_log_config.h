dad04b3cab277f2e3736947fc25e5f987b6a1f77
npc1054657282 <ly1054657282@gmail.com>
2025-05-09 02:51:12 +0800

    [doc]Fix readme doxygen. Add include guard for default `threadpool_log_config.h`.

--- src/threadpool_log_config.h
+++ src/threadpool_log_config.h
@@ -1,3 +1,6 @@
+#ifndef THREADPOOL_LOG_CONFIG_H
+#define THREADPOOL_LOG_CONFIG_H
+
 // MODIFY THIS FILE
 
 #include "utils/log.h"
@@ -7,3 +10,5 @@
 #define thpool_log_warn(fmt, ...)   log_warn(fmt __VA_OPT__(,) __VA_ARGS__)
 #define thpool_log_info(fmt, ...)   log_info(fmt __VA_OPT__(,) __VA_ARGS__)
 #define thpool_log_debug(fmt, ...)  log_debug(fmt __VA_OPT__(,) __VA_ARGS__)
+
+#endif
