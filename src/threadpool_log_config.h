2d5183e2780cbca2a6baf12b3503734e3d6b2b5a
npc1054657282 <56855941+npc1054657282@users.noreply.github.com>
2025-05-10 09:13:33 +0000

    Update threadpool_log_config.h for dynamic macro definitions based on environment

--- src/threadpool_log_config.h
+++ src/threadpool_log_config.h
@@ -5,10 +5,24 @@
 
 #include "utils/log.h"
 
+#if __STDC_VERSION__ >= 202311L
 #define thpool_log_fatal(fmt, ...)  log_fatal(fmt __VA_OPT__(,) __VA_ARGS__)
 #define thpool_log_error(fmt, ...)  log_error(fmt __VA_OPT__(,) __VA_ARGS__)
 #define thpool_log_warn(fmt, ...)   log_warn(fmt __VA_OPT__(,) __VA_ARGS__)
 #define thpool_log_info(fmt, ...)   log_info(fmt __VA_OPT__(,) __VA_ARGS__)
 #define thpool_log_debug(fmt, ...)  log_debug(fmt __VA_OPT__(,) __VA_ARGS__)
+#elif defined(__GNUC__) || defined(__clang__)
+#define thpool_log_fatal(fmt, ...)  log_fatal(fmt, ##__VA_ARGS__)
+#define thpool_log_error(fmt, ...)  log_error(fmt, ##__VA_ARGS__)
+#define thpool_log_warn(fmt, ...)   log_warn(fmt, ##__VA_ARGS__)
+#define thpool_log_info(fmt, ...)   log_info(fmt, ##__VA_ARGS__)
+#define thpool_log_debug(fmt, ...)  log_debug(fmt, ##__VA_ARGS__)
+#else
+#define thpool_log_fatal log_fatal
+#define thpool_log_error log_error
+#define thpool_log_warn log_warn
+#define thpool_log_info log_info
+#define thpool_log_debug log_debug
+#endif
 
 #endif
