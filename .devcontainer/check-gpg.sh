66b556d3cc5bb71a48f1a9d0d054ad3088f92436
npc1054657282 <56855941+npc1054657282@users.noreply.github.com>
2025-05-11 14:23:10 +0000

    [chore/codespace]Add check-gpg hint to `.bashrc` so that the hint will be more markedness.

--- .devcontainer/check-gpg.sh
+++ .devcontainer/check-gpg.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
+script_content=$(cat <<EOF
 if (! git config commit.gpgsign >/dev/null 2>&1;) && (! git config --global commit.gpgsign >/dev/null 2>&1;) then
-  cat << 'EOF'
-
-🚨 **Reminder:**  
-To ensure your PR isn’t blocked, please:
-
-  1. Go to **Settings → Codespaces → GPG verification** and **Enable** it.  
-  2. Under **Trusted repositories**, add this repository.  
-  3. **Stop & Restart** your Codespace so the new setting takes effect.
-
-EOF
-else echo -e "\n✅ Git Commit GPG Sign is enabled.\n"
+  echo -e "\n🚨 **Reminder:**  \n\nTo ensure your PR isn’t blocked, please:\n  1. Go to **Settings → Codespaces → GPG verification** and **Enable** it.  \n  2. Under **Trusted repositories**, add this repository.\n  3. **Stop & Restart** your Codespace so the new setting takes effect.\n"
+else
+  echo -e "\n✅ Git Commit GPG Sign is enabled.\n"
 fi
+EOF
+)
+echo "$script_content" >> ~/.bashrc
