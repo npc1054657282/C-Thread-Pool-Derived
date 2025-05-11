bdfb9b0fa1bca628ff5c759615775b254a73764d
npc1054657282 <56855941+npc1054657282@users.noreply.github.com>
2025-05-11 15:29:33 +0000

    [fix]Add g++-10 to let xmake recognize gcc-10.

--- .devcontainer/update-gcc.sh
+++ .devcontainer/update-gcc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa -y
-sudo apt install gcc-10 -y
-echo "export CC=gcc-10" >> ~/.bashrc
+sudo apt install gcc-10 g++-10 -y
+echo -e "export CC=gcc-10\nexport CXX=g++-10" >> ~/.bashrc
